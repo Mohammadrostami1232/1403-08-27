896b01378fa5211a759472dea6c1edddc4398538
Mohammadrostami1232 <148641750+Mohammadrostami1232@users.noreply.github.com>
2024-11-12 00:58:01 +0330

    Add files via upload

---
+++ Form12.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public abstract class vehicle
+        {
+            public abstract void Pedal();
+        }
+        public class Car : vehicle
+        {
+            public override void Pedal()
+            {
+                MessageBox.Show("the car acceleration");
+            }
+        }
+        public class Bike : vehicle
+        {
+            public override void Pedal()
+            {
+                MessageBox.Show("push pedal");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Car car = new Car();
+            Bike bike = new Bike();
+            car.Pedal();
+            bike.Pedal();
+        }
+    }
+}
